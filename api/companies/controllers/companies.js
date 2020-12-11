ec9ed50c5af7828334247322b87c696453395dec
Felipe Franco <felipe.franco@bizcap.com.br>
2020-12-11 20:16:57 -0300

    validações

--- api/companies/controllers/companies.js
+++ api/companies/controllers/companies.js
@@ -1,8 +1,30 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async create(ctx) {        
+        // você não pode se vincular a outra empresa
+        if (ctx.state.user.company) {
+            return ctx.throw(400, 'Você já está vinculado a uma empresa.');
+        }
+
+        // uma empresa só pode ser criada vinculada ao usuário logado
+        ctx.request.body.users = [ctx.state.user.id];
+
+        //você não pode vincular um usuario que está vinculado a outra empresa        
+        // const user = await strapi.query('user', 'users-permissions').findOne({ id: userId});
+        // if (user.company) {
+        //     return ctx.throw(400, 'Este usuário já está vinculado a uma empresa.');
+        // }
+        
+        const entity = await strapi.services.companies.create(ctx.request.body);
+        return sanitizeEntity(entity, { model: strapi.models.companies });
+    },
+
+    // implementar o update
+};
--- api/links/controllers/links.js
+++ api/links/controllers/links.js
@@ -1,8 +1,35 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+    async create(ctx) {
+        // você só pode criar um link na sua propria empresa
+        ctx.request.body.company = ctx.state.user.company;
+        const entity = await strapi.services.links.create(ctx.request.body);
+        return sanitizeEntity(entity, { model: strapi.models.links });
+    },
+
+    async find(ctx) {
+        // você só pode buscar os links vinculados a sua empresa
+        const company = ctx.state.user.company;        
+        
+        let entities;
+        ctx.query['company'] = company;
+        if (ctx.query._q) {
+          console.log('Entrou aqui: search');
+          entities = await strapi.services.links.search(ctx.query);
+        } else {
+          console.log('Entrou aqui: find');
+          entities = await strapi.services.links.find(ctx.query);
+        }
+    
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.links }));
+    },
+    
+};
