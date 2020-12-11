5d8b6d328bb3858c100d13e518eee848a6f10bc7
Felipe Franco <felipe.franco@bizcap.com.br>
2020-12-11 14:52:27 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '05d3b64db8f41d28093431372594c692'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e4a0c376-dc87-46d9-a9ca-0f31f722c2f4'
+};
