dd4dc3c6c92c194ad10237cdee5a750108e3eb9f
Felipe <felipe@fabricadecodigo.com>
2021-07-09 11:53:10 -0300

    ajuste

--- config/policies/hasCompany.js
+++ config/policies/hasCompany.js
@@ -5,7 +5,7 @@
  */
 
 module.exports = async (ctx, next) => {
-  if (!ctx.state.user.company) {
+  if (ctx.state.user && !ctx.state.user.company) {
     return ctx.unauthorized('Você não está vinculado a nenhuma empresa');
   }
 
